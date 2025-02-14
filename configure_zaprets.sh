15c139bb5f59fd439360ab2afb6d64ee590d3124
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-02-15 01:32:47 +0500

    Add files via upload

--- configure_zaprets.sh
+++ configure_zaprets.sh
@@ -7,20 +7,57 @@ config_files="dhcp
 youtubeUnblock
 https-dns-proxy"
 
+echo "Backup files..."
 
-mkdir $DIR_BACKUP
+if [ ! -d "$DIR_BACKUP" ]
+then
+  mkdir $DIR_BACKUP
+fi
 
 for file in $config_files
 do
   cp -f "$DIR/$file" "$DIR_BACKUP/$file"  
 done
 
+echo "Replace configs..."
+
 for file in $config_files
 do
-  wget -O "$DIR/$file" "$URL/$file" 
+  if [ "$file" != "dhcp" ] 
+  then 
+    wget -O "$DIR/$file" "$URL/$file" 
+  fi
 done
 
+echo "Configure dhcp..."
+
+uci set dhcp.cfg01411c.strictorder='1'
+uci set dhcp.cfg01411c.filter_aaaa='1'
+uci del dhcp.cfg01411c.server
+uci add_list dhcp.cfg01411c.server='127.0.0.1#5053'
+uci add_list dhcp.cfg01411c.server='127.0.0.1#5054'
+uci add_list dhcp.cfg01411c.server='127.0.0.1#5055'
+uci add_list dhcp.cfg01411c.server='127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.chatgpt.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.oaistatic.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.oaiusercontent.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.openai.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.microsoft.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.windowsupdate.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.bing.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.supercell.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.seeurlpcl.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.supercellid.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.supercellgames.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.clashroyale.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.brawlstars.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.clash.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.clashofclans.com/127.0.0.1#5056'
+uci commit dhcp
+
+echo "Restart service..."
 
 service youtubeUnblock restart
 service https-dns-proxy restart
 service dnsmasq restart
+service odhcpd restart
--- off_configure_zaprets.sh
+++ off_configure_zaprets.sh
@@ -6,11 +6,16 @@ config_files="dhcp
 youtubeUnblock
 https-dns-proxy"
 
+echo "Restore configs..."
+
 for file in $config_files
 do
   cp -f "$DIR_BACKUP/$file" "$DIR/$file"   
 done
 
+echo "Restart service..."
+
 service youtubeUnblock restart
 service https-dns-proxy restart
 service dnsmasq restart
+service odhcpd restart
