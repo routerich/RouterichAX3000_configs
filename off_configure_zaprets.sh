f4d73022bc671e3b24648834a9fb1484ee0ea7ae
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-02-14 21:48:48 +0500

    Add files via upload

--- off_configure_zaprets.sh
+++ off_configure_zaprets.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-URL="https://raw.githubusercontent.com/CodeRoK7/RouterichAX3000_configs/refs/heads/main"
 DIR="/etc/config"
 DIR_BACKUP="/root/backup"
 config_files="dhcp
 youtubeUnblock
 https-dns-proxy"
-
-
-mkdir $DIR_BACKUP
 
 for file in $config_files
 do
