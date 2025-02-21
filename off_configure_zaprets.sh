1aa929cbf148830f8d865f9c85d1a54d3496d5a2
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-02-21 22:09:02 +0500

    Delete cron task auto run script

--- off_configure_zaprets.sh
+++ off_configure_zaprets.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+URL_REPO="https://raw.githubusercontent.com/CodeRoK7/RouterichAX3000_configs"
 DIR="/etc/config"
 DIR_BACKUP="/root/backup"
 config_files="dhcp
@@ -19,5 +20,11 @@ service youtubeUnblock restart
 service https-dns-proxy restart
 service dnsmasq restart
 service odhcpd restart
+
+echo "Remove cron task auto run script configure zaprets.."
+
+grep -v "0 4 \* \* \* wget -O - $URL_REPO/refs/heads/main/configure_zaprets.sh | sh" /etc/crontabs/root > /etc/crontabs/temp
+cp -f "/etc/crontabs/temp" "/etc/crontabs/root"
+rm -f "/etc/crontabs/temp"
 
 echo "Off configure complete..."
