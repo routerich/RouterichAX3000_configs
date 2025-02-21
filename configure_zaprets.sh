64720b811a8baa7e906df54fd74aac23511262c5
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-02-21 09:36:57 +0500

    Add block QUIC and add new AI domain

--- configure_zaprets.sh
+++ configure_zaprets.sh
@@ -59,6 +59,8 @@ uci add_list dhcp.cfg01411c.server='/*.clashroyale.com/127.0.0.1#5056'
 uci add_list dhcp.cfg01411c.server='/*.brawlstars.com/127.0.0.1#5056'
 uci add_list dhcp.cfg01411c.server='/*.clash.com/127.0.0.1#5056'
 uci add_list dhcp.cfg01411c.server='/*.clashofclans.com/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.x.ai/127.0.0.1#5056'
+uci add_list dhcp.cfg01411c.server='/*.grok.com /127.0.0.1#5056'
 uci add dhcp domain # =cfg13f37d
 uci set dhcp.@domain[-1].name='chatgpt.com'
 uci set dhcp.@domain[-1].ip='94.131.119.85'
@@ -66,6 +68,24 @@ uci add dhcp domain # =cfg14f37d
 uci set dhcp.@domain[-1].name='openai.com'
 uci set dhcp.@domain[-1].ip='94.131.119.85'
 uci commit dhcp
+
+echo "Add block QUIC..."
+
+uci add firewall rule # =cfg2492bd
+uci set firewall.@rule[-1].name='Block_UDP_80'
+uci add_list firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].src='lan'
+uci set firewall.@rule[-1].dest='wan'
+uci set firewall.@rule[-1].dest_port='80'
+uci set firewall.@rule[-1].target='REJECT'
+uci add firewall rule # =cfg2592bd
+uci set firewall.@rule[-1].name='Block_UDP_443'
+uci add_list firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].src='lan'
+uci set firewall.@rule[-1].dest='wan'
+uci set firewall.@rule[-1].dest_port='443'
+uci set firewall.@rule[-1].target='REJECT'
+uci commit firewall
 
 echo "Crod task add restart service yotubeUnblock..."
 
