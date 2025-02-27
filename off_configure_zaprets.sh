577df6b406755079f8eb5ec726b9664f86bcc213
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-02-27 16:25:02 +0000

    Change repo

--- off_configure_zaprets.sh
+++ off_configure_zaprets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL="https://raw.githubusercontent.com/routerich/RouterichAX3000_configs/refs/heads/main"
+URL="https://raw.githubusercontent.com/CodeRoK7/RouterichAX3000_configs/refs/heads/main"
 DIR="/etc/config"
 DIR_BACKUP="/root/backup"
 config_files="dhcp
