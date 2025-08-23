f5ff284f19b6cb899712e1a371ec642ef1f8ca05
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-08-23 18:52:55 +0000

    Fix check version firmware

--- run_universal_config.sh
+++ run_universal_config.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-VERSION=$(ubus call system board | jsonfilter -e '@.release.description')
+DESCRIPTION=$(ubus call system board | jsonfilter -e '@.release.description')
+VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
+findKey="Routerich"
+findVersion="24.10.2"
 
-if echo "$VERSION" | grep -qi 'RouteRich'; then
+if echo "$DESCRIPTION" | grep -qi -- "$findKey" && printf '%s\n%s\n' "$findVersion" "$VERSION" | sort -V | tail -n1 | grep -qx -- "$VERSION"; then
 	printf "\033[32;1mThis new firmware. Running new scprit...\033[0m\n"
 	wget --no-check-certificate -O /tmp/universal_config_new_podkop.sh https://raw.githubusercontent.com/routerich/RouterichAX3000_configs/refs/heads/beta_alt_test/universal_config_new_podkop.sh && chmod +x /tmp/universal_config_new_podkop.sh && /tmp/universal_config_new_podkop.sh $1 $2
 else
