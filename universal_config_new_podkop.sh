0bb498e210ae223ed6e6c562858a4178b99b7e51
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2026-05-13 11:50:01 +0000

    Requered min version 24.10.5

--- universal_config_new_podkop.sh
+++ universal_config_new_podkop.sh
@@ -592,7 +592,7 @@ fi
 DESCRIPTION=$(ubus call system board | jsonfilter -e '@.release.description')
 VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
 findKey="RouteRich"
-findVersion="24.10.2"
+findVersion="24.10.5"
 
 if echo "$DESCRIPTION" | grep -qi -- "$findKey" && printf '%s\n%s\n' "$findVersion" "$VERSION" | sort -V | tail -n1 | grep -qx -- "$VERSION"; then
 	printf "\033[32;1mThis new firmware. Running scprit...\033[0m\n"
